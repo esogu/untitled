1389ce17b2c346acdb4eca58b62b161739080f11
esogu <drgrzm@gmail.com>
2014-12-14 21:45:27 +0200

    kodu revize ettik

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,8 +1,8 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("S.A. Dunya ");
-        System.out.println("\nKubra ");
+
+        System.out.println("\nKubra nasilsin ");
 
     }
 
